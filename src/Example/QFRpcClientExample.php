789379f6be6297d50c0811600da1ffde6dbc8f0f
dangcingzzw <dangcingzzw@163.com>
2024-01-02 15:45:34 +0800

    init

--- src/Example/QFRpcClientExample.php
+++ src/Example/QFRpcClientExample.php
@@ -7,7 +7,7 @@ use QfRPC\YARRPC\QFRpcClient;
 $ak = "9624cd0312cc2f1ad4fcc47366ca116c";
 $sk = "1acd91b8de88864baee20f998a985b5c1cfde75e503341bc4752be06c966a53d";
 
-//可访问的server文件地址(如果地址有效，浏览器访问会出现yar
+//可访问的server文件地址(如果地址有效，浏览器访问会出现yarServer列表
 $endpoint = 'http://127.0.0.1/QFRpcServerExample.php';
 
 $client = (new QFRpcClient($ak, $sk))
